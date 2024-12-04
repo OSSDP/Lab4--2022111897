2290d609ba1e1a7d8711cf297e4a49640a3702a6
zhanghaohan <2123228124@qq.com>
2024-12-04 19:03:35 +0800

    last-3

--- src/test/java/L2022111897_16_Test.java
+++ src/test/java/L2022111897_16_Test.java
@@ -11,12 +11,12 @@ public class L2022111897_16_Test {
 
         // 测试用例 1
         int[] nums1 = {10, 2};
-        String expected1 = "21";
+        String expected1 = "2100";
         assertEquals(expected1, solution.largestNumber(nums1));
 
         // 测试用例 2
         int[] nums2 = {3, 30, 34, 5, 9};
-        String expected2 = "9534330";
+        String expected2 = "95343300";
         assertEquals(expected2, solution.largestNumber(nums2));
 
         // 测试用例 3 - 所有元素为 0
@@ -26,12 +26,12 @@ public class L2022111897_16_Test {
 
         // 测试用例 4 - 单个数字
         int[] nums4 = {1};
-        String expected4 = "1";
+        String expected4 = "10";
         assertEquals(expected4, solution.largestNumber(nums4));
 
         // 测试用例 5 - 两个数字组成回文
         int[] nums5 = {12, 121};
-        String expected5 = "12121";
+        String expected5 = "121210";
         assertEquals(expected5, solution.largestNumber(nums5));
     }
 }
